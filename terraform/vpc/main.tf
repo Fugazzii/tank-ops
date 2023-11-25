0b8115e17f4ff66e8527c8c211901264f25e602b
Fugazzii <sichinavailia@gmail.com>
2023-11-25 16:04:01 +0400

    Added default security group

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -12,6 +12,17 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_security_group" "default" {
+    vpc_id = aws_vpc.main.id
+
+    ingress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 # Create public subnet
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
